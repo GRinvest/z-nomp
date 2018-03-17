29e99a4afffef2e3db124fac00ec538bea182fd5
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-03-17 16:00:15 +0200

    Update shareProcessor.js

--- libs/shareProcessor.js
+++ libs/shareProcessor.js
@@ -1,103 +1,122 @@
-var redis = require('redis');
-var Stratum = require('stratum-pool');
+var fs = require('fs');
+var dateFormat = require('dateformat');
+var colors = require('colors');
 
+JSON.minify = JSON.minify || require("node-json-minify");
 
-
-/*
-This module deals with handling shares when in internal payment processing mode. It connects to a redis
-database and inserts shares with the database structure of:
-
-key: coin_name + ':' + block_height
-value: a hash with..
-        key:
-
- */
-
-
-
-module.exports = function(logger, poolConfig){
-
-    var redisConfig = poolConfig.redis;
-    var coin = poolConfig.coin.name;
-
-
-    var forkId = process.env.forkId;
-    var logSystem = 'Pool';
-    var logComponent = coin;
-    var logSubCat = 'Thread ' + (parseInt(forkId) + 1);
-    
-    var connection = redis.createClient(redisConfig.port, redisConfig.host);
-    if (redisConfig.password) {
-        connection.auth(redisConfig.password);
+var severityToColor = function(severity, text) {
+    switch(severity) {
+        case 'special':
+            return text.cyan.underline;
+        case 'debug':
+            return text.green;
+        case 'warning':
+            return text.yellow;
+        case 'error':
+            return text.red;
+        default:
+            console.log("Unknown severity " + severity);
+            return text.italic;
     }
-    connection.on('ready', function(){
-        logger.debug(logSystem, logComponent, logSubCat, 'Share processing setup with redis (' + redisConfig.host +
-            ':' + redisConfig.port  + ')');
-    });
-    connection.on('error', function(err){
-        logger.error(logSystem, logComponent, logSubCat, 'Redis client had an error: ' + JSON.stringify(err))
-    });
-    connection.on('end', function(){
-        logger.error(logSystem, logComponent, logSubCat, 'Connection to redis database has been ended');
-    });
-    connection.info(function(error, response){
-        if (error){
-            logger.error(logSystem, logComponent, logSubCat, 'Redis version check failed');
-            return;
-        }
-        var parts = response.split('\r\n');
-        var version;
-        var versionString;
-        for (var i = 0; i < parts.length; i++){
-            if (parts[i].indexOf(':') !== -1){
-                var valParts = parts[i].split(':');
-                if (valParts[0] === 'redis_version'){
-                    versionString = valParts[1];
-                    version = parseFloat(versionString);
-                    break;
-                }
-            }
-        }
-        if (!version){
-            logger.error(logSystem, logComponent, logSubCat, 'Could not detect redis version - but be super old or broken');
-        }
-        else if (version < 2.6){
-            logger.error(logSystem, logComponent, logSubCat, "You're using redis version " + versionString + " the minimum required version is 2.6. Follow the damn usage instructions...");
-        }
-    });
+};
 
-    this.handleShare = function(isValidShare, isValidBlock, shareData) {
+var severityValues = {
+    'debug': 1,
+    'warning': 2,
+    'error': 3,
+    'special': 4
+};
 
-        var redisCommands = [];
+var conflog = JSON.parse(JSON.minify(fs.readFileSync("./config.json", {encoding: 'utf8'})));
+var logDir = conflog.logging.files.directory;
 
-        if (isValidShare) {
-            redisCommands.push(['hincrbyfloat', coin + ':shares:roundCurrent', shareData.worker, shareData.difficulty]);
-            redisCommands.push(['hincrby', coin + ':stats', 'validShares', 1]);
-        } else {
-            redisCommands.push(['hincrby', coin + ':stats', 'invalidShares', 1]);
+
+if (!fs.existsSync(logDir)){
+    try {
+        fs.mkdirSync(logDir);
+    }
+    catch(e){
+        throw e;
+    }
+}
+
+var pendingWrites = {};
+
+setInterval(function(){
+    for (var fileName in pendingWrites){
+        var data = pendingWrites[fileName];
+        fs.appendFileSync(fileName, data);
+        delete pendingWrites[fileName];
+    }
+}, conflog.logging.files.flushInterval * 1000);
+
+var PoolInfoLog = function (severity ,system, text, component, dateentry) {
+    var fileName = logDir + '/' + system + '_' + severity + '.log';
+    var fileLine = dateentry + ' ['+ system +'] ' + '['+ component +'] - ' + text + '\n';
+    pendingWrites[fileName] = (pendingWrites[fileName] || '') + fileLine;
+}
+
+var PoolLogger = function (configuration) {
+
+
+    var logLevelInt = severityValues[configuration.logLevel];
+    var logColors = configuration.logColors;
+
+
+
+    var log = function(severity, system, component, text, subcat) {
+
+        if (severityValues[severity] < logLevelInt) return;
+
+        if (subcat){
+            var realText = subcat;
+            var realSubCat = text;
+            text = realText;
+            subcat = realSubCat;
         }
+        var dateentry = dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss');
+        var entryDesc = dateentry + ' [' + system + ']\t';
+        if (logColors) {
+            entryDesc = severityToColor(severity, entryDesc);
 
-        /* Stores share diff, worker, and unique value with a score that is the timestamp. Unique value ensures it
-           doesn't overwrite an existing entry, and timestamp as score lets us query shares from last X minutes to
-           generate hashrate for each worker and pool. */
-        var dateNow = Date.now();
-        var hashrateData = [ isValidShare ? shareData.difficulty : -shareData.difficulty, shareData.worker, dateNow];
-        redisCommands.push(['zadd', coin + ':hashrate', dateNow / 1000 | 0, hashrateData.join(':')]);
+            var logString =
+                    entryDesc +
+                    ('[' + component + '] ').italic;
 
-        if (isValidBlock){
-            redisCommands.push(['rename', coin + ':shares:roundCurrent', coin + ':shares:round' + shareData.height]);
-            redisCommands.push(['rename', coin + ':shares:timesCurrent', coin + ':shares:times' + shareData.height]);
-            redisCommands.push(['sadd', coin + ':blocksPending', [shareData.blockHash, shareData.txHash, shareData.height, shareData.worker, dateNow].join(':')]);
-            redisCommands.push(['hincrby', coin + ':stats', 'validBlocks', 1]);
+            if (subcat)
+                logString += ('(' + subcat + ') ').bold.grey;
+
+            logString += text.grey;
         }
-        else if (shareData.blockHash){
-            redisCommands.push(['hincrby', coin + ':stats', 'invalidBlocks', 1]);
-        }
+        else {
+            var logString =
+                    entryDesc +
+                    '[' + component + '] ';
 
-        connection.multi(redisCommands).exec(function(err, replies){
-            if (err)
-                logger.error(logSystem, logComponent, logSubCat, 'Error with share processor multi ' + JSON.stringify(err));
-        });
+            if (subcat)
+                logString += '(' + subcat + ') ';
+
+            logString += text;
+        }
+		
+        if(conflog.logging.auth)
+		    PoolInfoLog(severity ,system, text, component, dateentry);
+        
+        console.log(logString);
+
+
     };
 
+    // public
+
+    var _this = this;
+    Object.keys(severityValues).forEach(function(logType){
+        _this[logType] = function(){
+            var args = Array.prototype.slice.call(arguments, 0);
+            args.unshift(logType);
+            log.apply(this, args);
+        };
+    });
 };
+
+module.exports = PoolLogger;
